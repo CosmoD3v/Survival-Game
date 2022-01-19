6840f02c6d831efef1ac790c8119134aaf4096ca
Se1an0s <selanosrl@gmail.com>
2022-01-19 00:01:34 -0500

    Options Menu

--- Survive/Scripts/OptionsMenu.gd
+++ Survive/Scripts/OptionsMenu.gd
@@ -1,14 +1,13 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var button = $CenterContainer/TabContainer/Graphics/GridContainer/Button
+var fullscreen : bool = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	button.text = "Fullscreen Off"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +17,15 @@ func _ready():
 
 func _on_Back_button_up():
 	Game.change_scene("MainMenu")
+
+
+func _on_Button_button_up():
+	change_fullscreen()
+
+func change_fullscreen():
+	fullscreen = not fullscreen
+	if fullscreen:
+		button.text = "Fullscreen On"
+	else:
+		button.text = "Fullscreen Off"
+	OS.window_fullscreen = fullscreen
