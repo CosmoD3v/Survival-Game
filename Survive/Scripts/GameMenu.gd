ee945ea0c433507cc328d23d86fa3be975cb9e60
CosmoD3v <cosmoexe404@gmail.com>
2022-01-22 12:10:55 -0600

    Major Progress Update
    Updated minor stuff and Overhauled the UI

--- Survive/Scripts/GameMenu.gd
+++ Survive/Scripts/GameMenu.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Control
 
 var player: Node
 
@@ -9,9 +9,11 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		$CenterContainer/MenuGrid.visible = !$CenterContainer/MenuGrid.visible
+		get_tree().paused = !get_tree().paused
 
 func _on_Resume_button_up() -> void:
 	$CenterContainer/MenuGrid.visible = false
+	get_tree().paused = !get_tree().paused
 
 func _on_Quit_button_up() -> void:
 	Game.save_user_data(player.inventoryNode.inventory)
